2c1d64c3d6c6a1d220f953843e09690fb9458b3f
Caoyang Shi <CShi@walmartlabs.com>
2016-01-12 02:21:54 -0800

    separate tests

--- test/index.js
+++ test/index.js
@@ -5,16 +5,18 @@ const csrfPlugin = require("../").hapiPlugin;
 
 const chai = require("chai");
 const expect = chai.expect;
-
 const jwt = require("jsonwebtoken");
 
+let server;
+const secret = "test";
+
 describe("test csrf-jwt hapi plugin", () => {
-  it("return view with jwt", (done) => {
-    const server = new Hapi.Server();
+  before(() => {
+    server = new Hapi.Server();
     server.connection();
 
     const options = {
-      secret: "test",
+      secret,
       expiresIn: "2d",
       ignoreThisParam: "ignore"
     };
@@ -47,41 +49,59 @@ describe("test csrf-jwt hapi plugin", () => {
             method: "post",
             path: "/2",
             handler: (request, reply) => {
-              expect(request.payload).to.deep.equal({message: "hello", jwt: request.plugins.jwt});
-
+              expect(request.payload.message).to.equal("hello");
+              expect(request.payload.jwt).to.equal(request.plugins.jwt);
               return reply("valid");
             }
           }
         ]);
-
-        server.inject({method: "get", url: "/1"}, (res) => {
-          const token = res.request.plugins.jwt;
-          expect(res.payload).to.contain(token);
-          expect(res.payload).to.contain("hi");
-
-          server.inject({method: "post", url: "/2", payload: {message: "hello", jwt: token}}, (res) => {
-            expect(res.statusCode).to.equal(200);
-            expect(res.result).to.equal("valid");
-
-            server.inject({method: "post", url: "/2", payload: {message: "hello"}}, (res) => {
-              expect(res.statusCode).to.equal(403);
-
-              const token = jwt.sign({ip: "123.123.123.123"}, options.secret, {});
-
-              server.inject({method: "post", url: "/2", payload: {message: "hello", jwt: token}}, (res) => {
-                expect(res.statusCode).to.equal(403);
-
-                server.inject({method: "post", url: "/2", payload: {message: "hello", jwt: "123"}}, (res) => {
-                  expect(res.statusCode).to.equal(403);
-                });
-              });
-            });
-
-          });
-
-          done();
-        });
       });
     });
+  });
+
+  it("return success", (done) => {
+    return server.inject({method: "get", url: "/1"})
+      .then((res) => {
+        const token = res.request.plugins.jwt;
+        expect(res.statusCode).to.equal(200);
+        expect(res.payload).to.contain(token);
+        expect(res.payload).to.contain("hi");
+        return server.inject({method: "post", url: "/2", payload: {message: "hello", jwt: token}})
+          .then((res) => {
+            expect(res.statusCode).to.equal(200);
+            expect(res.result).to.equal("valid");
+            done();
+          });
+      })
+      .catch((err) => {
+        expect(err).to.not.exist;
+        done();
+      });
+  });
+
+  it("return 500 for missing jwt", (done) => {
+    server.inject({method: "post", url: "/2", payload: {message: "hello"}})
+      .then((err) => {
+        expect(err.statusCode).to.equal(500);
+        done();
+      });
+  });
+
+  it("return 500 for wrong ip", (done) => {
+    const token = jwt.sign({ip: "123.123.123.123"}, secret, {});
+
+    server.inject({method: "post", url: "/2", payload: {message: "hello", jwt: token}})
+      .then((err) => {
+        expect(err.statusCode).to.equal(500);
+        done();
+      });
+  });
+
+  it("return 500 for invalid jwt", (done) => {
+    server.inject({method: "post", url: "/2", payload: {message: "hello", jwt: "123"}})
+      .then((err) => {
+        expect(err.statusCode).to.equal(500);
+        done();
+      });
   });
 });
