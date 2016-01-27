06cfa1b0d62130aa97b3ec7217b06638fb6f3ecf
Caoyang Shi <CShi@walmartlabs.com>
2016-01-27 11:18:38 -0800

    fix hapi tests

--- test/csrf-hapi-test.js
+++ test/csrf-hapi-test.js
@@ -52,9 +52,8 @@ describe("test csrf-jwt hapi plugin", () => {
             method: "get",
             path: "/1",
             handler: (request, reply) => {
-              expect(request.headers.jwt).to.exist;
-
-              return reply.view("index", {message: "hi", jwt: request.headers.jwt});
+              expect(request.jwt).to.exist;
+              return reply.view("index", {message: "hi", jwt: request.jwt});
             }
           },
           {
@@ -62,7 +61,7 @@ describe("test csrf-jwt hapi plugin", () => {
             path: "/2",
             handler: (request, reply) => {
               expect(request.payload.message).to.equal("hello");
-              expect(request.payload.headers.jwt).to.equal(request.headers.jwt);
+              expect(request.payload.jwt).to.equal(request.jwt);
               return reply("valid");
             }
           }
@@ -74,16 +73,21 @@ describe("test csrf-jwt hapi plugin", () => {
   it("should return success", (done) => {
     return server.inject({method: "get", url: "/1"})
       .then((res) => {
-        const token = res.request.headers.jwt;
+        const token = res.request.jwt;
         expect(res.statusCode).to.equal(200);
-        expect(res.payload).to.contain(token);
         expect(res.payload).to.contain("hi");
-        return server.inject({method: "post", url: "/2", payload: {message: "hello", jwt: token}})
-          .then((res) => {
-            expect(res.statusCode).to.equal(200);
-            expect(res.result).to.equal("valid");
-            done();
-          });
+        expect(res.headers["x-csrf-token"]).to.equal(token);
+        expect(res.headers["set-cookie"][0]).to.contain("jwt=");
+        return server.inject({
+          method: "post",
+          url: "/2",
+          payload: {message: "hello"},
+          headers: {"x-csrf-token": token, Cookie: res.headers["set-cookie"][0]}
+        }).then((res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.result).to.equal("valid");
+          done();
+        });
       })
       .catch((err) => {
         expect(err).to.not.exist;
@@ -99,22 +103,26 @@ describe("test csrf-jwt hapi plugin", () => {
       });
   });
 
-  it("should return 500 for wrong ip", (done) => {
-    const token = jwt.sign({ip: "123.123.123.123"}, secret, {});
-
-    server.inject({method: "post", url: "/2", payload: {message: "hello", jwt: token}})
-      .then((err) => {
-        expect(err.statusCode).to.equal(500);
-        done();
-      });
-  });
-
   it("should return 500 for invalid jwt", (done) => {
-    const token = jwt.sign({ip: "127.0.0.1"}, "ssh");
-
-    server.inject({method: "post", url: "/2", payload: {message: "hello", jwt: token}})
-      .then((err) => {
-        expect(err.statusCode).to.equal(500);
+    return server.inject({method: "get", url: "/1"})
+      .then((res) => {
+        const token = res.request.jwt;
+        expect(res.statusCode).to.equal(200);
+        expect(res.payload).to.contain("hi");
+        expect(res.headers["x-csrf-token"]).to.equal(token);
+        expect(res.headers["set-cookie"][0]).to.contain("jwt=");
+        return server.inject({
+          method: "post",
+          url: "/2",
+          payload: {message: "hello"},
+          headers: {"x-csrf-token": token, Cookie: `jwt=${token}`}
+        }).then((res) => {
+          expect(res.statusCode).to.equal(500);
+          done();
+        });
+      })
+      .catch((err) => {
+        expect(err).to.not.exist;
         done();
       });
   });
